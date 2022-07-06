e6ea4fd27fcca1908cc71715f00816563d4e951a
Bancho <gnr11matos@gmail.com>
2022-07-06 21:26:21 +0100

    Add DELETE methods

--- deck_management.py
+++ deck_management.py
@@ -5,7 +5,7 @@ from models import Deck, Card, db
 
 decks = Blueprint(name="decks", import_name=__name__, url_prefix="/decks", template_folder='templates')
 
-@decks.route("/create_deck", methods=['POST'])
+@decks.route("/decks", methods=['POST'])
 @login_required
 def add_deck():
     deck_data = request.json
@@ -25,11 +25,19 @@ def get_decks():
         dictionary[d.name] = [(c.name, c.power, c.description) for c in d.cards]
     return dictionary
 
-@decks.route("/deck/<id>")
+@decks.route("/deck/<id>", methods=['GET'])
 @login_required
 def get_deck(id):
-    deck = (Deck.query.filter(Deck.user == current_user)).filter_by(user_id=id).first()
+    deck = get_by_user_id(id, Deck)
     return {"name": deck.name, "cards": [c.name for c in deck.cards]}
+
+@decks.route("/deck/<id>", methods=['DELETE'])
+@login_required
+def delete_deck(id):
+    deck = get_by_user_id(id, Deck)
+    db.session.delete(deck)
+    db.session.commit()
+    return {"deck_deleted": deck.name}
 
 @decks.route("/cards", methods=['GET'])
 @login_required
@@ -37,7 +45,7 @@ def get_cards():
     cards = current_user.cards
     return {c.user_id : c.name for c in cards}
 
-@decks.route("/add_card", methods=['POST'])
+@decks.route("/cards", methods=['POST'])
 @login_required
 def add_card_to_user():
     card_data = request.json
@@ -49,10 +57,10 @@ def add_card_to_user():
     db.session.commit()
     return {"card_added": card.name}
 
-@decks.route("/add_card_to_deck/<id>", methods=['POST'])
+@decks.route("/card/<id>", methods=['POST'])
 @login_required
 def add_card_to_deck(id):
-    deck = (Deck.query.filter(Deck.user == current_user)).filter_by(user_id=id).first()
+    deck = get_by_user_id(id, Deck)
     card_data = request.json
     power = card_data['power']
     name = card_data['name']
@@ -62,11 +70,22 @@ def add_card_to_deck(id):
     db.session.commit()
     return {"card_added": card.name, "deck": deck.name}
 
-@decks.route("/remove_card", methods=['DELETE'])
+@decks.route("/card/<id>", methods=['DELETE'])
 @login_required
-def remove_card_from_user():
-    
+def remove_card_from_user(id):
+    card = get_by_user_id(id, Card)
+    db.session.delete(card)
+    db.session.commit()
+    return {"card_deleted": card.name}
 
+@decks.route("/card/<deck_id>/<card_id>", methods=['DELETE'])
+@login_required
+def remove_card_from_deck(deck_id, card_id):
+    deck = get_by_user_id(deck_id, Deck)
+    card = (Card.query.filter(Card.deck == deck)).filter_by(deck_id=card_id).first()    
+    db.session.delete(card)
+    db.session.commit()
+    return {"card_deleted": card.name}
 
-#@decks.route("/delete", methods=['DELETE'])
-#def delete
+def get_by_user_id(id, model):
+    return (model.query.filter(model.user == current_user)).filter_by(user_id=id).first()
